adeaa473106ee34a03e28507bd3dcf3f131af798
Martin Zlocha <zlocha.martin@gmail.com>
2015-05-03 22:53:44 +0200

    Fixing Auto Update

--- Anivia.lua
+++ Anivia.lua
@@ -2,12 +2,12 @@ local version = "1.1"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/gmzopper/BoL/blob/master/Anivia.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = LIB_PATH.."Anivia.lua"
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function _AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Anivia:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/gmzopper/BoL/blob/master/version/Anivia.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/gmzopper/BoL/master/version/Anivia.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
