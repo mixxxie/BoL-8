76185f0eb587f855b2d4eaf08e776596180f7c8e
Martin Zlocha <zlocha.martin@gmail.com>
2015-05-14 20:23:50 +0200

    Reverted changes

--- Ashe.lua
+++ Ashe.lua
@@ -1,4 +1,4 @@
-local version = "1.0"
+local version = "1.03"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/gmzopper/BoL/master/Ashe.lua".."?rand="..math.random(1,10000)
